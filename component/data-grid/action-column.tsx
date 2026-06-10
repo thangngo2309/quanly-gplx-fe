2124c5f1fbfe0cdf3b2cc5681a535d7f703a937e
Nga To <19574802010125@vinhuni.edu.vn>
2026-06-10 10:18:17 +0700

    QG-48 add tooltip for icon

--- component/data-grid/action-column.tsx
+++ component/data-grid/action-column.tsx
@@ -3,6 +3,7 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { IconButton } from "@mui/material";
 import { ActionHandlers } from "@/model/grid-data/action-handlers";
+import Tooltip from '@mui/material/Tooltip';
 
 export const ActionColumn = <T,>({
     onEdit,
@@ -21,32 +22,36 @@ export const ActionColumn = <T,>({
 
             if (onEdit) {
                 actions.push(
-                    <IconButton color="primary" onClick={() => onEdit(row)}>
-                        <EditIcon />
-                    </IconButton>
+                    <Tooltip title="Chỉnh sửa">
+                        <IconButton color="primary" onClick={() => onEdit(row)}>
+                            <EditIcon />
+                        </IconButton>
+                    </Tooltip>
                 );
             }
 
             if (onDelete) {
                 actions.push(
-                    <IconButton
-                        color="error"
-                        onClick={() => {
-                            const label = getDeleteLabel?.(row);
-                            const id = getDeleteId?.(row);
+                    <Tooltip title="Xóa">
+                        <IconButton
+                            color="error"
+                            onClick={() => {
+                                const label = getDeleteLabel?.(row);
+                                const id = getDeleteId?.(row);
 
-                            if (
-                                id !== undefined &&
-                                confirm(
-                                    `Bạn có chắc chắn muốn xóa ${label}?`
-                                )
-                            ) {
-                                onDelete(id);
-                            }
-                        }}
-                    >
-                        <DeleteIcon />
-                    </IconButton>
+                                if (
+                                    id !== undefined &&
+                                    confirm(
+                                        `Bạn có chắc chắn muốn xóa ${label}?`
+                                    )
+                                ) {
+                                    onDelete(id);
+                                }
+                            }}
+                        >
+                            <DeleteIcon />
+                        </IconButton>
+                    </Tooltip>
                 );
             }
 
